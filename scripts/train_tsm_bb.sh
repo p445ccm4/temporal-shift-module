0b498722582b326aba4240bf0c491802794178f0
Michael <p445ccm4@connect.hku.hk>
2023-12-11 19:05:34 +0800

    Made another stable and squared dataset

--- scripts/train_tsm_bb.sh
+++ scripts/train_tsm_bb.sh
@@ -1,7 +1,9 @@
 # You should get TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth
+cd /media/nvidia/6561-3431/temporal-shift-module
+
 python3 main.py bb-dataset-cropped RGB \
      --arch resnet50 --num_segments 8 \
-     --gd 20 --lr 0.005 --wd 1e-4 --lr_steps 20 40 --epochs 10 \
+     --gd 20 --lr 0.005 --wd 1e-4 --lr_steps 20 40 --epochs 50 \
      --batch-size 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
      --shift --shift_div=8 --shift_place=blockres --npb \
      --tune_from=pretrained/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth

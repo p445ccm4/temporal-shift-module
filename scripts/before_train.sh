a7e85c690e96da6537c8ecdbf10a66fc19515313
Michael <p445ccm4@yahoo.com.hk>
2023-12-12 18:34:24 +0800

    +Dataset focusing on upper body. +Fix bug that use BGR images for infer. +Can crop a big bbox for 2-stage infer.

--- scripts/before_train.sh
+++ scripts/before_train.sh
@@ -1,5 +1,6 @@
 cd /media/nvidia/6561-3431/temporal-shift-module
 
+# make sure the dataset directory is correct before running the script
 python3 scripts/flip_dataset.py
 python3 scripts/gen_txt.py
 python3 scripts/reorder_frames.py
